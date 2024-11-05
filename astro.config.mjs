251ae6570647bcb8bd4b961c5462dd3c9dc88dd9
Abraham <41970794+ABeens@users.noreply.github.com>
2024-11-05 13:04:20 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(),tailwind()]
+  site: 'https://abeens.github.io',
+  base: '/Portfolio',
+  integrations: [mdx(), tailwind()]
 });
