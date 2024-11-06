0a00fb689db1018706e44f226f51d28ecb189534
Abraham <41970794+ABeens@users.noreply.github.com>
2024-11-05 20:15:21 -0600

    Ajuste de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://abeens.github.io/Portfolio',
-  integrations: [mdx(), tailwind()]
+  site: 'https://abeens.github.io',
+  base: '/Portfolio',
+  integrations: [tailwind()]
 });
