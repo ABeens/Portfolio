91b7d108f0530475ea1185b277ca918ca499e788
Abraham <41970794+ABeens@users.noreply.github.com>
2024-11-05 20:24:15 -0600

    Manejo correcto de imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://abeens.github.io',
   base: '/Portfolio',
+  site: 'https://abeens.github.io',
   integrations: [tailwind()]
 });
